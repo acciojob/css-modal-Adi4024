93cc9eabc7678b37de42c351a9e37d7b0f00f5ef
Adi4024 <166373177+Adi4024@users.noreply.github.com>
2025-11-01 19:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close-modal");
+
+// Open modal
+openBtn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// Close modal when clicking X
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+// Close when clicking outside modal
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
